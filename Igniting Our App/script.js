0ee9d2c97826092d8df89cff1028e1d898fcacca
Harsh91221 <harshch91221@gmail.com>
2024-01-20 19:34:19 +0530

    CODE

--- Igniting Our App/script.js
+++ Igniting Our App/script.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const heading = React.createElement("h1", { id: "heading" }, "Namaste");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
