b57eb0ea096c433304b559cea64e22a6f395e7a9
Harsh91221 <harshch91221@gmail.com>
2024-01-23 01:44:10 +0530

    CODE

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,15 @@
 import RestaurantCard from "./RestaurantCard";
 import resObj from "../utils/mockData";
+import { useState } from "react";
 const Body = () => {
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    resObj = resObj.filter((res) => res.info.avgRating > 4);
+                    console.log(resObj);
+                }}>Top Rated Restaurant</button>
+            </div>
             <div className="res-container">
                 {
                     resObj.map((restaurant) => (<RestaurantCard key={restaurant.info.id} resData={restaurant} />))
