3ff496837adfb8aaf831c3d8f35045e0c8556238
Harsh91221 <harshch91221@gmail.com>
2024-01-24 01:29:24 +0530

    CODE

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,10 +9,11 @@ const Body = () => {
     const fetchData = async () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4808184&lng=77.5177682&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
-        setlistofRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        console.log(json);
+        setlistofRestaurant(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
-    if(listofRestaurant.length==0){
-        return <Shimmer/>
+    if (listofRestaurant.length == 0) {
+        return <Shimmer />
     }
     return (
         <div className="body">
