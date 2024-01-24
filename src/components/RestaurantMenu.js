69e60ea392c4c7650a7471428cfb54c9274c85cf
Harsh91221 <harshch91221@gmail.com>
2024-01-25 01:24:33 +0530

    CODE

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,16 +1,17 @@
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
-
+import { useParams } from "react-router-dom";
 const RestaurantMenu = () => {
     const [resInfo, setResInfo] = useState(null);
-
+    const { resId } = useParams();
+    console.log(resId);
     useEffect(() => {
         fetchMenu();
     }, []);
 
     const fetchMenu = async () => {
         try {
-            const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.4808184&lng=77.5177682&restaurantId=28798&catalog_qa=undefined&submitAction=ENTER");
+            const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.4808184&lng=77.5177682&restaurantId=28798");
             const json = await data.json();
             console.log(json);
             setResInfo(json.data);
@@ -31,11 +32,7 @@ const RestaurantMenu = () => {
             <h1>{name}</h1>
             <h3>{cuisines.join(", ")} - {costForTwoMessage}</h3>
             <ul>
-                {itemCards.map((item) => (
-                    <li key={item.card.info.id}>
-                        {itemCards.map((item) => (<li>{item.card.info.name} - {"Rs."}{item.card.info.Price / 100 || item.card.info.defaultPrice / 100}</li>))}
-                    </li>
-                ))}
+                {itemCards.map((item) => (<li key={item.card.info.id}>{item.card.info.name} - {"Rs."}{item.card.info.price / 100 || item.card.info.defaultPrice / 100}</li>))}
             </ul>
         </div>
     );
