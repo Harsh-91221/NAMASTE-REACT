0727d5242090437f272b384abf04f3a419400583
Harsh91221 <harshch91221@gmail.com>
2024-01-24 22:56:11 +0530

    CODE

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/about";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const AppLayout = () => {
     return (
@@ -16,10 +18,15 @@ const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <AppLayout />,
+        errorElement:<Error/>,
     },
     {
         path: "/about",
         element: <About />,
+    },
+    {
+        path: "/contact",
+        element: <Contact />,
     },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
