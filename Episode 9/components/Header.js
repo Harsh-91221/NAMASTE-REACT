740dbcd093360542a3cd76e03ffe18d2e108ffa6
Harsh91221 <harshch91221@gmail.com>
2024-02-08 19:52:46 +0530

    CODE

---
+++ Episode 9/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+const Header = () => {
+    const [btnNameReact, setbtnNameReact] = useState("Login");
+    const onlineStatus = useOnlineStatus();
+    return (
+        <div className="header">
+            <div className="logoContainer">
+                <img className="logo" src={LOGO_URL}></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Online Status:{onlineStatus ? " 🟢" : " 🔴"}</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/About">About Us</Link></li>
+                    <li><Link to="/Contact">Contact Us</Link></li>
+                    <li>Cart</li>
+                    <button className="Login" onClick={() => { btnNameReact == "Login" ? setbtnNameReact("Logout") : setbtnNameReact("Login") }}>{btnNameReact}</button>
+                </ul>
+            </div>
+        </div>
+    )
+};
+export default Header;
