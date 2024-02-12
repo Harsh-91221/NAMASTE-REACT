80e17b98ea7bc8dfcc2410aa9e77f3a2852fbea1
Harsh91221 <harshch91221@gmail.com>
2024-02-12 18:36:02 +0530

    CODE

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,14 @@ import { useState ,useContext} from "react";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import userContext from "../utils/userContext";
+import { useSelector } from "react-redux";
 const Header = () => {
     const [btnNameReact, setbtnNameReact] = useState("Login");
     const onlineStatus = useOnlineStatus();
     const {loggedInUser}=useContext(userContext);
     console.log(loggedInUser);
+    const cartItems =useSelector((store)=>store.cart.items);
+    console.log(cartItems);
     return (
         <div className="flex justify-between bg-grey-100 shadow-lg sm:bg-yellow-50">
             <div className="logoContainer">
@@ -20,7 +23,7 @@ const Header = () => {
                     <li className="px-4"><Link to="/">Home</Link></li>
                     <li className="px-4"><Link to="/About">About Us</Link></li>
                     <li className="px-4"><Link to="/Contact">Contact Us</Link></li>
-                    <li className="px-4">Cart</li>
+                    <li className="px-4 font-bold text-xl"><Link to="/cart">Cart - ({cartItems.length})</Link></li>
                     <button className="Login" onClick={() => { btnNameReact == "Login" ? setbtnNameReact("Logout") : setbtnNameReact("Login") }}>{btnNameReact}</button>
                     <li className="px-4 font-bold">{loggedInUser}</li>
                 </ul>
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,12 @@
+import { useDispatch } from "react-redux";
 import { CDN_URL } from "../utils/constants";
+import { addItem } from "../utils/cartSlice";
 const ItemList = ({items}) => {
     console.log(items);
+    const dispatch=useDispatch();
+    const handleAddItems=(item)=>{
+        dispatch(addItem(item));
+    };
     return (<div>
         {items.map((item) => (
         <div key={item.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between">
@@ -13,7 +19,7 @@ const ItemList = ({items}) => {
             </div>
             <div className="w-3/12 p-4">
             <div className="absolute">
-            <button className="p-2 mx-50 rounded-lg bg-black text-white shadow-lg">Add+</button>
+            <button className="p-2 mx-50 rounded-lg bg-black text-white shadow-lg" onClick={()=>handleAddItems(item)}>Add+</button>
             </div>
             <img src={CDN_URL + item.card.info.imageId} className="w-full"/>
             </div>
