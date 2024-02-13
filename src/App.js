cd43550b1eaa1daf326f6fe5572a1fa46ce87b64
Harsh Chaudhary <113262189+Harsh-91221@users.noreply.github.com>
2024-02-14 00:16:04 +0530

    CODE (#2)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/about";
+import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
