97961b7a574b546415880df9f0284ac67368c91f
Harsh91221 <harshch91221@gmail.com>
2024-02-21 00:39:11 +0530

    CODE

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,10 @@
 import food from "../images/food-png.png"
 const About = () => {
   return (
-    <div className="min-h-screen">
-      <div className="mx-auto mt-20 text-center"></div>
-      <div className="flex flex-wrap justify-evenly items-center m-10 md:m-50">
-        <div className="flex flex-wrap flex-col justify-start items-start overflow-y-hidden">
+    <div className="about-container-main min-h-screen">
+      <div className="about-profile-container mx-auto mt-20 text-center"></div>
+      <div className="about-container flex flex-wrap justify-evenly items-center m-10 md:m-50">
+        <div className="about-left flex flex-wrap flex-col justify-start items-start overflow-y-hidden">
           <h1 className="text-7xl text-light-text-color">
            Step into <br /> Flavorful Fields of <br />
             <span className="inline-block bg-orange-500 px-4 py-1 rounded-lg text-light-white-text">Culinary Delight</span>
@@ -13,7 +13,7 @@ const About = () => {
             "Where Every Bite Tells a Story"
           </h4>
         </div>
-        <div className="flex flex-wrap flex-col justify-center items-center overflow-y-hidden">
+        <div className="about-right flex flex-wrap flex-col justify-center items-center overflow-y-hidden">
           <img src={food} alt="Food Image" className="w-[500]"/>
         </div>
       </div>
